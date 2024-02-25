3d3e95657e1a6d9185362264b605bd83489a41d8
Petar Markov <50070761+kivox@users.noreply.github.com>
2024-02-25 21:06:17 +0000

    users

--- src/pages/users.js
+++ src/pages/users.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import Layout from "@/components/Layout";
+import {useQuery} from "@tanstack/react-query";
 
 function Users(props) {
+    // Get the current logged in user's account data
+    const {data: accountData, isSuccess} = useQuery({
+        queryKey: ["/account"],
+    });
+
+    // Get a list of users on the platform
+    const {data: usersData, isSuccess: usersSuccess} = useQuery({
+        queryKey: ["/users"],
+    });
+
     return (
         <Layout title={"Users"}>
-
+            <div className={"space-y-2 py-4"}>
+                {/* Show the users if they have been fetched */}
+                {usersSuccess && usersData?.map((user) => (
+                    <div className={"flex flex-col p-4 border rounded-md"} key={user.id}>
+                        <p className={"text-base font-medium"}>{user.display_name}</p>
+                        <p className={"text-sm font-normal text-muted-foreground"}>@{user.username}</p>
+                    </div>
+                ))}
+            </div>
         </Layout>
     );
 }
